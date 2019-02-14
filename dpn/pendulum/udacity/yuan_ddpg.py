5b5bc371fbd337f834f187420683b79b0c0bca59
Yuan Chen <chenyuan@NYCnerds-MacBook-Air.local>
2019-02-13 21:33:43 -0500

    temp ddpg

---
+++ dpn/pendulum/udacity/yuan_ddpg.py
@@ -0,0 +1,27 @@
+import gym
+import random
+import torch
+import numpy as np
+from collections import deque
+import matplotlib.pyplot as plt
+
+from drl.dpn.agent.naive_ddpg import DDPGAgent
+from drl.framework.config import DDPGConfig
+from drl.util.score_tracker import ScoreTracker
+
+env = gym.make('Pendulum-v0')
+env.seed(2)
+config = DDPGConfig()
+config.env_driver = env
+
+config.soft_update_tau = 1e-3
+config.discount = 0.99
+config.score_tracker = ScoreTracker(good_target=100, window_len=100)
+
+
+agent = DDPGAgent(config)
+
+while True:
+    if agent.step():
+        break
+

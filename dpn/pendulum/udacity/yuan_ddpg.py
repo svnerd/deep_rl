4172e3ccfd136a9c41b1ebbd4e99957d6ffddab8
Yuan Chen <chenyuan@NYCnerds-MacBook-Air.local>
2019-02-13 21:38:19 -0500

    add noise

--- dpn/pendulum/udacity/yuan_ddpg.py
+++ dpn/pendulum/udacity/yuan_ddpg.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from drl.dpn.agent.naive_ddpg import DDPGAgent
 from drl.framework.config import DDPGConfig
 from drl.util.score_tracker import ScoreTracker
+from drl.util.noise import OUNoise
 
 env = gym.make('Pendulum-v0')
 env.seed(2)
@@ -17,6 +18,7 @@ config.env_driver = env
 config.soft_update_tau = 1e-3
 config.discount = 0.99
 config.score_tracker = ScoreTracker(good_target=100, window_len=100)
+config.noise = [OUNoise(1, seed=2)] * 1
 
 
 agent = DDPGAgent(config)

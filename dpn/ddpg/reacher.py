a8da3ea11f7b1a88b63456bbb99327d9ae5181b3
Yuan Chen <chenyuan@NYCnerds-MacBook-Air.local>
2019-03-15 16:58:40 -0400

    tieout 4

--- dpn/ddpg/reacher.py
+++ dpn/ddpg/reacher.py
@@ -10,6 +10,7 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("--os", default="linux", help="os")
 parser.add_argument("--graph", action="store_true")
+parser.add_argument("--good", action="store_true")
 args = parser.parse_args()
 
 BATCH_SIZE=128
@@ -22,24 +23,25 @@ dim_tensor_maker = SingleAgentDimTensorMaker(
     act_space=env.act_dim
 )
 
-#agent = ReacherAgent(env, dim_tensor_maker, BATCH_SIZE,random_seed=0)
-agent = Agent(state_size=env.obs_dim, action_size=env.act_dim, num_agents=1, random_seed=0)
+agent_good = Agent(state_size=env.obs_dim, action_size=env.act_dim, num_agents=1, random_seed=0)
+agent_bad = ReacherAgent(env, dim_tensor_maker, BATCH_SIZE,random_seed=0)
 
 score_tracker = ScoreTracker(good_target=100, window_len=100)
 for e in range(200):
     states, r, dones = env.reset()
     scores = np.zeros(env.num_agents)
-    agent.reset()
+    agent_good.reset()
+    agent_bad.reset()
     while True:
         # all actions between -1 and 1
-        # actions = agent.act(dim_tensor_maker.agent_in(obs=states))
-        actions = agent.act(states)
+        actions = agent_bad.act(dim_tensor_maker.agent_in(obs=states))
+        actions = agent_good.act(states)
         next_states, rewards, dones = env.step(actions)
         dim_tensor_maker.check_env_out(
             next_states, rewards, dones
         )
-        #agent.update(states, actions, next_states, rewards, dones)
-        agent.step(states, actions, rewards, next_states, dones)
+        agent_good.step(states, actions, rewards, next_states, dones)
+        agent_bad.update(states, actions, next_states, rewards, dones)
         scores += rewards                         # update the score (for each agent)
         states = next_states                               # roll over states to next time step
         if np.any(dones):                                  # exit loop if episode finished

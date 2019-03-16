4d49c7a51ead8314de056f5f640427ea87e9d97b
Yuan Chen <chenyuan@NYCnerds-MacBook-Air.local>
2019-03-15 22:05:57 -0400

    tie out 3

--- dpn/ddpg/ddpg_agent.py
+++ dpn/ddpg/ddpg_agent.py
@@ -39,6 +39,9 @@ def _make_actor_critic_net_udacity(obs_dim, act_dim, random_seed):
     ).to(device)
     return actor_net, critic_net
 
+def _soft_update(target_net, local_net, tau=1.0):
+    for target_param, local_param in zip(target_net.parameters(), local_net.parameters()):
+        target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
 
 class Agent():
     """Interacts with and learns from the environment."""
@@ -141,18 +144,5 @@ class Agent():
         self.actor_optimizer.step()
 
         # ----------------------- update target networks ----------------------- #
-        self.soft_update(self.critic_local, self.critic_target, TAU)
-        self.soft_update(self.actor_local, self.actor_target, TAU)                     
-
-    def soft_update(self, local_model, target_model, tau):
-        """Soft update model parameters.
-        θ_target = τ*θ_local + (1 - τ)*θ_target
-
-        Params
-        ======
-            local_model: PyTorch model (weights will be copied from)
-            target_model: PyTorch model (weights will be copied to)
-            tau (float): interpolation parameter 
-        """
-        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
+        _soft_update(self.critic_target, self.critic_local, TAU)
+        _soft_update(self.actor_target, self.actor_local, TAU)
